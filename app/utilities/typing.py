773594d4368a793aa3bbf6a75fed8a5a9cd78324
ViolaBuddy <ViolaBuddy@users.noreply.github.com>
2022-11-20 23:43:40 -0500

    undo initial clone

--- app/utilities/typing.py
+++
@@ -1,9 +0,0 @@
-from typing import Tuple
-
-Point = Tuple[int, int]
-Segment = Tuple[Point, Point]
-
-NID = str
-UID = int
-Color3 = Tuple[int, int, int]
-Color4 = Tuple[int, int, int, int]
--- utilities/build_tools/zipify.py
+++
@@ -1,9 +0,0 @@
-# zipify
-import os, shutil, sys
-
-directory = os.path.abspath(sys.argv[1])
-if os.path.isdir(directory):
-    print("Zipping %s..." % directory)
-    shutil.make_archive(directory, 'zip', directory)
-else:
-    print("Error! Could not locate %s" % directory)
